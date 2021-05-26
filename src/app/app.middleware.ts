df6aa14b6c75e3f98aaa9fd713f38ab7ee03e51e
Nilo Almeida <niloalmeidadev@gmail.com>
2021-05-26 10:43:54 -0300

    alterando estruturas de arquivos

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -1,7 +1,10 @@
 import express from 'express';
-import userMiddleware from '../user/user.middleware';
+import userMiddleware from '../user/middlewares/user.middleware';
+import taskMiddleware from '../task/middlewares/task.middleware';
 
 const middleware = express()
+
 middleware.use(userMiddleware)
+middleware.use(taskMiddleware)
 
 export default middleware;
